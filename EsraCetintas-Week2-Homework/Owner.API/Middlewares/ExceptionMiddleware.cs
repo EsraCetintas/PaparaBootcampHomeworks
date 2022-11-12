7ea8381fdb9609e7eeef6abc120437f967459fc7
Esra Çetintaş <Esra.Cetintas@outlook.com.tr>
2022-11-12 15:39:41 +0300

    Add files via upload

---
+++ EsraCetintas-Week2-Homework/Owner.API/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Owner.API.Middlewares
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+               await _next(httpContext);
+            }
+            catch (System.Exception ex)
+            {
+                httpContext.Response.StatusCode = 400;
+                await httpContext.Response.WriteAsync(ex.Message);
+            }
+        }
+    }
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+}
