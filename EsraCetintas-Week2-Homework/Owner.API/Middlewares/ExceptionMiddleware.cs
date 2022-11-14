853fed4b4f8ae80e65a01d144ebb0e30a1d161b7
Esra Çetintaş <Esra.Cetintas@outlook.com.tr>
2022-11-14 15:25:22 +0300

    Add files via upload

---
+++ EsraCetintas-Week2-Homework/Owner.API/Middlewares/ExceptionMiddleware.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Newtonsoft.Json;
+using Owner.API.Middlewares;
+using Owner.API.Middlewares.Model;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Owner.API.Middlewares
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            try
+            {
+               await _next(httpContext);
+            }
+            catch (System.Exception ex)
+            {
+                httpContext.Response.ContentType = "application/json";
+                var json = JsonConvert.SerializeObject(new APIResult { Message=ex.Message,StatusCode= (int)HttpStatusCode.BadRequest});
+                await httpContext.Response.WriteAsync(json);
+            }
+        }
+    }
+}
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+    public static class ExceptionMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseExceptionMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<ExceptionMiddleware>();
+        }
+    }
+
---
+++ EsraCetintas-Week2-Homework/Owner.API/Model/Owner.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+
+namespace Owner.API.Model
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime Date { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+    }
+}
